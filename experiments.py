cc662b148ea3ecf578baae428c7ce5f7274c8167
realsung <sungjun0208@naver.com>
2024-06-06 03:00:01 +0900

    add langchain utils

--- experiments.py
+++ experiments.py
@@ -11,6 +11,13 @@ import instructions
 import csv
 import sast_llm
 from helper_utils import *
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import OnlinePDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 # def create_database
 
@@ -61,7 +68,99 @@ def set_dataset_with_csv():
                 os.system(command2)   
                 print(command2)
 
+def langchain_data_load_with_url(url: str):
+    '''
+    URL을 통해서 외부 데이터를 가져온다.
+    '''
+    loader = WebBaseLoader(url)
+    
+    return loader.load()
+
+def langchain_data_load_with_bs4(url: tuple, class_name: tuple):
+    '''
+    url: tuple - (url, url, ...)
+    class_name: tuple - (class_name, class_name, ...)
+    bs4활용, URL을 통해서 외부 데이터를 가져온다.
+    '''
+    loader = WebBaseLoader(
+    web_paths = url,
+    bs_kwargs = dict(
+        parse_only = bs4.SoupStrainer(
+            class_ = class_name
+            )
+        ),
+    )
+    docs = loader.load()
+    return docs
+
+
+def langchain_data_load_with_textfile(file_path: str):
+    '''
+    텍스트 파일을 통해서 데이터를 가져온다.
+    '''
+    loader = TextLoader(file_path)
+    data =  loader.load()
+    return data
+
+def langchain_data_load_with_directory(directory_path: str, glob_opt: str):
+    '''
+    directory_path: str - 파일들을 가져올 디렉토리 경로
+    glob_opt: str - glob 옵션 (e.g. '*.txt', '*.csv', ...)
+    디렉토리를 통해서 데이터를 가져온다.
+    리스트 형태로 반환한다.
+    '''
+    loader = DirectoryLoader(directory_path, glob=glob_opt)
+    data = loader.load()
+    return data
+
+def langchain_data_load_with_csv(csv_path: str):
+    '''
+    csv 파일을 통해서 데이터를 가져온다.
+    '''
+    loader = CSVLoader(csv_path, encoding='cp949')
+    data = loader.load()
+    return data
+
+def langchain_data_load_with_pdf(pdf_path: str):
+    '''
+    pdf 파일을 통해서 데이터를 가져온다.
+    리스트 리턴
+    '''
+    loader = PyPDFLoader(pdf_path)
+    data = loader.load()
+    return data
+
+def langchain_data_load_with_online_pdf(url: str):
+    '''
+    온라인 pdf를 통해서 데이터를 가져온다.
+    '''
+    loader = OnlinePDFLoader(url)
+    data = loader.load()
+    return data[0]
+
+def langchain_data_load_with_pdf_directory(directory_path: str, glob_opt: str):
+    '''
+    pdf 디렉토리를 통해서 데이터를 가져온다.
+    '''
+    loader = PyPDFDirectoryLoader(directory_path)
+    data = loader.load()
+    return data
+
+
+def langchain_data_split(data: str):
+    '''
+    데이터를 분할한다.
+    '''
+    splitter = RecursiveCharacterTextSplitter() # RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    # return splitter.split(data)
+    print(splitter.split_documents(data))
+
+
 def main():
+    a = langchain_data_load_with_url('https://cwe.mitre.org/data/definitions/78.html')
+    print(len(a[0].page_content))
+    go = langchain_data_split(a)
+    print(go)
 
     '''
     zero-shot 패치 실험
@@ -70,7 +169,7 @@ def main():
 
     RAG fine-tuning을 이용한 취약점 패치 실험
     1. 데이터셋 csv를 통해서 취약점 줄에 comment를 추가한다.
-    2. 해당 취약점에 대한 CWE 외부 정보 langchain(보류) RAG fine-tuning을 진행한다.
+    2. 해당 취약점에 대한 CWE 외부 정보 langchain 기반 RAG를 진행한다.
     3. 취약점 코드에 패치를 적용한다.
 
     zeroshot 패치와 RAG fine-tuning 패치의 성능을 비교한다.
